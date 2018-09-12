67e9619b506c51b39839d323a3201765e6c3d817
scottyd_14@hotmail.com <scottyd_14@hotmail.com>
2018-09-12 15:47:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,20 +77,11 @@ function removeFromCart(item) {
 removeFromCart('skis');
   
   
-function placeOrder(cardNumber) {
-  if(cardNumber) 
-  {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
-    for (var i = cart.length; i>= 0; i--)
-    { cart.pop();} 
-  }
-  else 
-  {
-    console.log('Sorry, we don\'t have a credit card on file for you.')
-    return cart;
-  }
+function placeOrder() {
+ 
+
 }
 
-placeOrder();
+
 
 
