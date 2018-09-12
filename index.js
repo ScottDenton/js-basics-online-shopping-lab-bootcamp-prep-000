f9aaec94eb1c63e9720967f877ec43dd4ea47df0
scottyd_14@hotmail.com <scottyd_14@hotmail.com>
2018-09-12 15:47:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,41 +9,9 @@ function addToCart (item) {
   console.log(`${item} has been added to your cart.`);
   return cart;
 }
-
-
-addToCart('glove')
-addToCart('ball')
-addToCart('skis')
-addToCart('shoes')
-
 function viewCart() {
-  var cartItems = [];
-  if (cart.length === 0) {
-    console.log('Your shopping cart is empty.')
-
-  } else if (cart.length === 1) {
-    var printOneItem = `In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`
-    console.log(printOneItem)
-
-  } else if (cart.length === 2) {
-    var printTwoItems = `In your cart, you have ${Object.keys(cart[cart.length-2])} at $${cart[cart.length-2][Object.keys(cart[cart.length-2])]} and ${Object.keys(cart[cart.length-1])} at $${cart[cart.length-1][Object.keys(cart[cart.length-1])]}.`
-    console.log(printTwoItems)
   
-    
-  } else {
-      for (var i = 0; i < cart.length-1; i++)
-      {
-        //console.log(cart[i]);
-        cartItems.push(` ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}`);
-      }
-      
-      var printThreeOrMoreItems = `In your cart, you have${cartItems}, and ${Object.keys(cart[cart.length-1])} at $${cart[cart.length-1][Object.keys(cart[cart.length-1])]}.`
-      console.log(printThreeOrMoreItems);
-  }
 }
-
-viewCart(cart);
-
 
 function total() {
   
